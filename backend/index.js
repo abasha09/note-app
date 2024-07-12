6ee779ab57c64e5d1c485eb46a3e8ca778cfc9fb
irakli abashidze <irakli.abashidzeee@gmail.com>
2024-07-12 13:31:23 +0400

    msg

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const Note = require("./models/note.model");
 
 // APP CONFIG
 const express = require("express");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 
 const cors = require("cors");
 const app = express();
